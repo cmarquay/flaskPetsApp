ece853e0e1db9bd1254f38971fa26d661c9d392c
Christian MARQUAY <christianmarquay@hotmail.com>
2020-06-28 14:40:46 +0200

    Added the connection of the Model Server to the app

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_bootstrap import Bootstrap
+
+import os
+import app.inference as inference
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -12,4 +15,11 @@ Routes
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        uploaded_file = request.files["file"]
+        if uploaded_file.filename != "":
+            image_path = os.path.join("app/static", uploaded_file.filename)
+            uploaded_file.save(image_path)
+            class_name = inference.get_prediction(image_path)
+            print("CLASS NAME = ", class_name)
     return render_template("index.html")
